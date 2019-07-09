fbb7d01f8e305d373b2aa558e2972c5cab3e3ad0
Vozneichuk <vozneichuk@gmail.com>
2019-07-09 18:01:06 +0300

    add task 9 and 11

--- hw js1.js
+++ hw js1.js
@@ -79,3 +79,108 @@ function cursorCheck(str){
 } 
 		return false;
 }
+
+//TASK 9
+
+function toUppercase(str) {
+    let arr = str.split("");
+    for(let i = 0; i<arr.length; i++){
+      switch(arr[i]){
+        case 'q':
+          arr[i] = "Q";
+            break;
+        case 'w':
+					arr[i] = "W";
+					break;
+        case 'e':
+					arr[i] = "E";
+					break;
+        case 'r':
+					arr[i] = "R";
+					break;
+        case 't':
+					arr[i] = "T";
+					break;
+        case 'y':
+					arr[i] = "Y";
+					break;
+        case 'u':
+					arr[i] = "U";
+					break;
+        case 'i':
+					arr[i] = "I";
+					break;
+        case 'o':
+					arr[i] = "O";
+					break;
+        case 'p':
+					arr[i] = "P";
+					break;
+        case 'a':
+					arr[i] = "A";
+					break;
+        case 's':
+					arr[i] = "S";
+					break;
+        case 'd':
+					arr[i] = "D";
+					break;
+        case 'f':
+					arr[i] = "F";
+					break;
+        case 'g':
+					arr[i] = "G";
+					break;
+        case 'h':
+					arr[i] = "H";
+					break;
+        case 'j':
+					arr[i] = "J";
+					break;
+        case 'k':
+					arr[i] = "K";
+					break;
+        case 'l':
+					arr[i] = "L";
+					break;
+        case 'z':
+					arr[i] = "Z";
+					break;
+        case 'x':
+					arr[i] = "X";
+					break;
+        case 'c':
+					arr[i] = "C";
+					break;
+        case 'v':
+					arr[i] = "V";
+					break;
+        case 'b':
+					arr[i] = "B";
+					break;
+        case 'n':
+					arr[i] = "N";
+					break;
+        case 'm':
+					arr[i] = "M";
+					break;
+        default:
+					arr[i] = arr[i];
+					break;
+        }
+    }
+    return arr.join("");
+}
+
+
+//TASK 11
+
+function fibonacci(n){
+	let a = 1, b = 1;
+	for(let i = 3; i <= n; i++){
+		const c = a + b;
+		a = b;
+		b = c;
+	}
+	return b;
+}
